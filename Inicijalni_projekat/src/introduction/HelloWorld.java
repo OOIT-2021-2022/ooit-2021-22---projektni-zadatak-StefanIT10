4136f2dfe5833b4b14fb2c8a77a7de94143465fc
IT10-2021 <IT10-2021@AR4-08.mshome.net>
2022-03-14 08:15:42 +0100

    Druge vezbe prvi deo

--- Inicijalni_projekat/src/introduction/HelloWorld.java
+++ Inicijalni_projekat/src/introduction/HelloWorld.java
@@ -4,8 +4,25 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		
+		//zadatak 2
+		
+		/* float faktorijel = 1;
+		int n=5, i=1;
+		
+		while(i<=n) {
+			faktorijel*=i;
+			i++;
+		}
+		System.out.println(faktorijel);*/
+		
+		//zadatak 3
+		
+		for(int i=1;i<10;i++) {
+		if(i%2==0)
+			System.out.println("\n" +i);
 
 	}
 
+}
 }

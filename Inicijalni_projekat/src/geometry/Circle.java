7580c612308f99f009c5d6dd1bdc522da8e69b00
IT10-2021 <IT10-2021@AR4-08.mshome.net>
2022-03-21 08:50:37 +0100

    trece vezbe

---
+++ Inicijalni_projekat/src/geometry/Circle.java
@@ -0,0 +1,39 @@
+package geometry;
+
+public class Circle {
+	
+	private Point center;
+	private int radius;
+	
+	public double area() {
+		return radius*radius*Math.PI;
+	}
+	
+	public double circumference() {
+		return 2*radius*Math.PI;
+	}
+	
+	
+	public Point getCenter() {
+		return this.center;
+	}
+	
+	public void setCenter(Point center) {
+		this.center=center;
+		
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	
+	
+	
+	
+
+}
--- Inicijalni_projekat/src/geometry/test2.java
+++ Inicijalni_projekat/src/geometry/test2.java
@@ -13,7 +13,64 @@ public class test2 {
 		
 		p.setY(20);
 		
-		System.out.println("Udaljenost je: "+p.distance(15, 25));
+		System.out.println("Udaljenost je: " +p.distance(15, 25));
+		
+		
+		//vezbe 3
+		//point p => x=10, y=20
+		p.setSelected(true);
+		Point p1= new Point();
+		
+		p1.setX(20);
+		p1.setY(25);
+		
+		line l1 = new line();
+		System.out.println(l1.getStartPoint()+ " "+ l1.getEndPoint());
+		System.out.println(l1.isSelected());
+		
+		Rectangle r1=new Rectangle();
+		Circle c1 = new Circle();
+		
+		
+		System.out.println(p.getX());
+		p.setX(p1.getY());
+		System.out.println(p.getX());
+		
+		
+	    l1.setStartPoint(p);
+	    l1.setEndPoint(p1);
+	    System.out.println(l1.getEndPoint().getX()+ " "+ l1.getStartPoint().getY());
+	    System.out.println(l1.getEndPoint().getX()+ " "+ l1.getEndPoint().getY());
+	    
+	    
+	    l1.getEndPoint().setY(23);
+	    l1.getStartPoint().setX(l1.getEndPoint().getY());
+	    l1.getEndPoint().setX((int)(l1.length()-(l1.getStartPoint().getX()+l1.getStartPoint().getY())));
+	    
+	  
+	    
+	    //r1.getUpperLeftPoint().setX(10);
+	    r1.setUpperLeftPoint(p);
+	   // r1.setUpperLeftPoint(new Point(p.getX(),p.getY()));  ignorisi
+	    r1.getUpperLeftPoint().setX(10);
+	    r1.getUpperLeftPoint().setY(15);
+	    
+	    
+	    
+	    
+	    c1.setCenter(r1.getUpperLeftPoint());
+	    
+	   
+	    r1.setHeight(10);
+	    r1.setWidth(15);
+	    c1.getCenter().setX(r1.area()-l1.getStartPoint().getY());
+	    
+		
+		
+		
+		
+		
+		
 
 	}
 

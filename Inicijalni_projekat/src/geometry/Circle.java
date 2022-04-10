644796929596817d4391166fdfe3d86f144d52cb
Stefan <101461603+StefanIT10@users.noreply.github.com>
2022-04-10 23:30:26 +0200

    Update Circle.java

--- Inicijalni_projekat/src/geometry/Circle.java
+++ Inicijalni_projekat/src/geometry/Circle.java
@@ -5,6 +5,35 @@ public class Circle {
 	private Point center;
 	private int radius;
 	
+	public Circle() {
+
+	}
+
+	public Circle(point center, int radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+
+	public Circle(point center, int radius, boolean selected) {
+		this(center, radius);
+		this.selected = selected;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Circle) {
+			Cirlce pomocna = (Circle) obj; //downcast
+			if (this.center.equals(pomocni.center) &&
+					this.radius == pomocni.radius)
+				return true;
+			
+			else 
+				return false;
+			
+		} else
+			return false;
+		
+		}
+	
 	public double area() {
 		return radius*radius*Math.PI;
 	}
@@ -29,6 +58,11 @@ public class Circle {
 
 	public void setRadius(int radius) {
 		this.radius = radius;
+	}
+	
+	public String toString() {
+		// Center=(x,y), radius= radius
+		return "Center=" + center + ", radius=" + radius;
 	}
 	
 
